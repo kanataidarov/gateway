417b74c7cf702bacbc0883cee0b9db753a9c0d03
k.aidarov <k.aidarov@infin-it.kz>
2020-02-21 12:07:49 +0600

    Initial version of gateway generated by JHipster-6.6.0

---
+++ src/main/java/kz/kaznu/nmm/aglomer/config/GatewayConfiguration.java
@@ -0,0 +1,32 @@
+package kz.kaznu.nmm.aglomer.config;
+
+import io.github.jhipster.config.JHipsterProperties;
+
+import kz.kaznu.nmm.aglomer.gateway.accesscontrol.AccessControlFilter;
+import kz.kaznu.nmm.aglomer.gateway.responserewriting.SwaggerBasePathRewritingFilter;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Configuration
+    public static class SwaggerBasePathRewritingConfiguration {
+
+        @Bean
+        public SwaggerBasePathRewritingFilter swaggerBasePathRewritingFilter(){
+            return new SwaggerBasePathRewritingFilter();
+        }
+    }
+
+    @Configuration
+    public static class AccessControlFilterConfiguration {
+
+        @Bean
+        public AccessControlFilter accessControlFilter(RouteLocator routeLocator, JHipsterProperties jHipsterProperties){
+            return new AccessControlFilter(routeLocator, jHipsterProperties);
+        }
+    }
+
+}
